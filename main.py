b877ea4b1e0bcc43110b0756d2864d7dbc5e6ea4
Jr Teles <jrteles.moreira@gmail.com>
2026-05-02 18:45:35 -0400

    refactor: Chrome pool 3->4, stale timeout 5->7min

--- main.py
+++ main.py
@@ -566,7 +566,7 @@ def _split_routes(routes: list[RouteQuery], chunks: int) -> list[list[RouteQuery
 
 
 _CHROME_SEMAPHORE_PATH = "/tmp/vooindo_chrome_semaphore"
-_CHROME_MAX_CONCURRENT = 3  # 3 Chromes simultâneos (reduzido de 5 para evitar trap int3)
+_CHROME_MAX_CONCURRENT = 4  # 4 Chromes simultâneos
 
 # Conjunto de aeroportos brasileiros para timeout dinâmico
 _BR_CODES: set[str] = {
