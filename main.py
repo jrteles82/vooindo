7203fa090aee12f63aa17bcaad920a02d5c81ec8
Jr Teles <jrteles.moreira@gmail.com>
2026-05-02 10:45:32 -0400

    fix: chrome crash trap int3 + reduz concurrent 5->3
    - Adiciona --disable-crashpad aos args do Chrome
    - Reduz _CHROME_MAX_CONCURRENT de 5 para 3
    - Minimiza crashes de assertion interna do chromium
      (trap int3) quando multiplas instancias sobem

--- main.py
+++ main.py
@@ -566,7 +566,7 @@ def _split_routes(routes: list[RouteQuery], chunks: int) -> list[list[RouteQuery
 
 
 _CHROME_SEMAPHORE_PATH = "/tmp/vooindo_chrome_semaphore"
-_CHROME_MAX_CONCURRENT = 5  # 5 Chromes simultâneos (5 workers)
+_CHROME_MAX_CONCURRENT = 3  # 3 Chromes simultâneos (reduzido de 5 para evitar trap int3)
 
 # Conjunto de aeroportos brasileiros para timeout dinâmico
 _BR_CODES: set[str] = {
